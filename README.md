e4a04c0cc197032ab4c3d5d1787e1eef376f23ba
ygbingo <yanhuibin315@163.com>
2023-06-13 03:18:48 +0000

    add delete script

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ A python script to detect duplicate documents in Elasticsearch. Once duplicates 
 
 For a full description on how this script works including an analysis of the memory requirements, see: https://alexmarquardt.com/2018/07/23/deduplicating-documents-in-elasticsearch/
 
+在原始脚本基础上，添加删除重复文档的实现逻辑，且要留下一个文档
--- deduplicate-elaticsearch.py
+++ deduplicate-elaticsearch.py
@@ -3,6 +3,9 @@
 # A description and analysis of this code can be found at 
 # https://alexmarquardt.com/2018/07/23/deduplicating-documents-in-elasticsearch/
 
+# 删除es里重复的文档
+
+from loguru import logger
 import hashlib
 from elasticsearch import Elasticsearch, helpers
 
@@ -15,7 +18,17 @@ dict_of_duplicate_docs = {}
 
 # The following line defines the fields that will be
 # used to determine if a document is a duplicate
+# 筛选的重复字段
 keys_to_include_in_hash = ["CAC", "FTSE", "SMI"]
+
+# 查找重复字段的索引，可以加通配符*
+scan_index = "stocks*"
+
+# 删除文档的索引，不可以加通配符
+mget_index = "stock"
+
+# 根据具体es的内容得到
+doc_type = "_doc"
 
 
 # Process documents returned by the current search/scroll
@@ -41,7 +54,7 @@ def populate_dict_of_duplicate_docs(hit):
 # Loop over all documents in the index, and populate the
 # dict_of_duplicate_docs data structure.
 def scroll_over_all_docs():
-    for hit in helpers.scan(es, index='stocks'):
+    for hit in helpers.scan(es, index=scan_index):
         populate_dict_of_duplicate_docs(hit)
 
 
@@ -49,22 +62,24 @@ def loop_over_hashes_and_remove_duplicates():
     # Search through the hash of doc values to see if any
     # duplicate hashes have been found
     for hashval, array_of_ids in dict_of_duplicate_docs.items():
-      if len(array_of_ids) > 1:
-        print("********** Duplicate docs hash=%s **********" % hashval)
-        # Get the documents that have mapped to the current hasval
-        matching_docs = es.mget(index="stocks", doc_type="doc", body={"ids": array_of_ids})
-        for doc in matching_docs['docs']:
-            # In order to remove the possibility of hash collisions,
-            # write code here to check all fields in the docs to
-            # see if they are truly identical - if so, then execute a
-            # DELETE operation on all except one.
-            # In this example, we just print the docs.
-            print("doc=%s\n" % doc)
-
+        if len(array_of_ids) > 1:
+            logger.info("********** Duplicate docs hash=%s **********" % hashval)
+            # Get the documents that have mapped to the current hasval
+            matching_docs = es.mget(index=mget_index, doc_type=doc_type, body={"ids": array_of_ids})
+            count = len(matching_docs['docs'])
+            for doc in matching_docs['docs']:
+                # 删除一个重复文档后弹出
+                logger.info("doc=%s\n" % doc)
+                es.delete(index=doc['_index'], id=doc['_id'])
+                count -= 1
+                logger.info(f"delete index: {doc['_index']} _id: {doc['_id']}")
+                if count <= 1: break
 
 
 def main():
+    logger.info("查找全部文档...")
     scroll_over_all_docs()
+    logger.info("删除重复文档...")
     loop_over_hashes_and_remove_duplicates()
 
 
